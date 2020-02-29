b20a82a83e4e4d447e6f54c6c474a95cb35dd5b4
Johnathan James <haxwell@gmail.com>
2020-02-28 18:09:28 -0700

    Initial commit, a copy pasta from tech-profile-editor.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'display',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'editor',
+    loadChildren: () => import('./editor/editor.module').then( m => m.EditorPageModule)
+  },
+  {
+    path: 'question-manager',
+    loadChildren: () => import('./question-manager/question-manager.module').then( m => m.QuestionManagerPageModule)
+  },
+  {
+    path: 'display',
+    loadChildren: () => import('./display/display.module').then( m => m.DisplayPageModule)
   }
 ];
 
